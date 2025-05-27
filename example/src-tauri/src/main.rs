ac1ffce8fb2b5461bb2d001ee57c5f7b2c539a44
Egor Kolesnikov <freethinkel@gmail.com>
2025-05-27 13:07:53 +0500

    moved to new version and update deps

--- example/src-tauri/src/main.rs
+++ example/src-tauri/src/main.rs
@@ -1,8 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{tray::{MouseButton, MouseButtonState, TrayIcon, TrayIconEvent}, ActivationPolicy, Manager};
-use tauri_plugin_nspopover::{AppExt, WindowExt};
+use tauri::{
+    tray::{MouseButton, MouseButtonState, TrayIcon, TrayIconEvent},
+    ActivationPolicy, Manager,
+};
+use tauri_plugin_nspopover::{AppExt, ToPopoverOptions, WindowExt};
 
 fn main() {
     tauri::Builder::default()
@@ -11,25 +14,28 @@ fn main() {
         .setup(|app| {
             app.set_activation_policy(ActivationPolicy::Accessory);
             let window = app.handle().get_webview_window("main").unwrap();
-            window.to_popover();
+            window.to_popover(ToPopoverOptions {
+                is_fullsize_content: true,
+            });
 
             let tray = app.tray_by_id("main").unwrap();
             let handle = app.handle().clone();
 
-
-            tray.on_tray_icon_event(move |_, event| {
-              match event {
-                TrayIconEvent::Click { button, button_state, .. } => {
-                  if button == MouseButton::Left && button_state == MouseButtonState::Up {
-                      if !handle.is_popover_shown() {
-                          handle.show_popover();
-                      } else {
-                          handle.hide_popover();
-                      }
-                  }
-                },
+            tray.on_tray_icon_event(move |_, event| match event {
+                TrayIconEvent::Click {
+                    button,
+                    button_state,
+                    ..
+                } => {
+                    if button == MouseButton::Left && button_state == MouseButtonState::Up {
+                        if !handle.is_popover_shown() {
+                            handle.show_popover();
+                        } else {
+                            handle.hide_popover();
+                        }
+                    }
+                }
                 _ => {}
-              }
             });
 
             Ok(())
--- example/src/main.ts
+++ example/src/main.ts
@@ -1,19 +1,27 @@
 import { TrayIcon } from "@tauri-apps/api/tray";
 import { isOpen, show, hide } from "tauri-plugin-nspopover";
 
-const main = () => {
+const main = async () => {
   TrayIcon.new({
     id: "main",
-    async action() {
-      const isShown = await isOpen();
-      if (isShown) {
-        hide();
-      } else {
-        show();
+    async action(event) {
+      console.log(event);
+      if (
+        event.type === "Click" &&
+        event.buttonState === "Up" &&
+        event.button === "Left"
+      ) {
+        const isShown = await isOpen();
+
+        if (isShown) {
+          hide();
+        } else {
+          show();
+        }
       }
     },
   });
 };
 
 // uncomment and disable rust code to handle popover in javascript
-// main()
+// main();
