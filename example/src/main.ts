8d1b2883a27551af1c51a2cc6eb4c4f983e19aaf
Egor Kolesnikov <freethinkel@gmail.com>
2025-05-27 13:16:24 +0500

    cleanup & fix readme

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { TrayIcon } from "@tauri-apps/api/tray";
 import { isOpen, show, hide } from "tauri-plugin-nspopover";
 
@@ -26,28 +25,3 @@ const main = async () => {
 
 // uncomment and disable rust code to handle popover in javascript
 // main();
-||||||| af9b6d0
-=======
-import { invoke } from "@tauri-apps/api/tauri";
-
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
->>>>>>> main
